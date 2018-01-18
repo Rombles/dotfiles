87c5316fc531fe74e91ddd9277073980677062a7
Jeffrey Slabaugh <jeffrey.slabaugh@gmail.com>
2018-01-18 01:10:50 -0500

    Fixed some pathing issues wth pyenv

--- python/env.sh
+++ python/env.sh
@@ -1,8 +1,12 @@
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH=$PYENV_ROOT/bin:$PATH
 
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
-fi
+# enable pyenv and pyenv-virtualenv
+if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+
+#if command -v pyenv 1>/dev/null 2>&1; then
+#  eval "$(pyenv init -)"
+#  eval "$(pyenv virtualenv-init -)"
+#fi
 
